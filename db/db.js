25b1c12447a409e02835f16bcbdae4f25f4c9135
sanjaytamilan0 <sanjaytamilan0@gmail.com>
2024-08-16 22:40:26 +0530

    no change

--- db/db.js
+++ db/db.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 mongoose.connect("mongodb://127.0.0.1:27017/musicapp")
   .then(() => {
     console.log("connected to db");
+    console.log("connected to db");
   })
   .catch((error) => {
     console.log("error connecting to the db", error);
